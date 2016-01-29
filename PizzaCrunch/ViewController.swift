f38181457fe58ebabe2181262588cfe9ef0feb42
ahadsheriff <ahadsheriff@gmail.com>
2016-01-29 17:51:34 -0500

    Display HealthData on Label
    Wrote the generic code for updating the label in the app with Active
    Energy data

--- PizzaCrunch/ViewController.swift
+++ PizzaCrunch/ViewController.swift
@@ -16,7 +16,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Asks the user to allow access to HealthKit data
+        
         let healthStore = HKHealthStore()
+        
+        
+        // Start and end date are the same because we want to collect data within the same day
+        let startDate = NSDate()
+        let endDate = NSDate()
         
         if let energyType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierActiveEnergyBurned) {
             
@@ -24,6 +31,40 @@ class ViewController: UIViewController {
             healthStore.requestAuthorizationToShareTypes(setType, readTypes: setType, completion: { (success, error) -> Void in
             })
         }
+        
+        if let energyType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierActiveEnergyBurned) {
+            
+            // Predicate so that we collect data from within the same day
+            
+            let timeFrame = HKQuery.predicateForSamplesWithStartDate(startDate, endDate: endDate, options: .None)
+            
+            // Sort descriptor to get most recent data first
+            
+            let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+            
+            // Execute query and gather Health data
+            
+            healthStore.executeQuery(HKSampleQuery(sampleType: energyType, predicate: timeFrame, limit: 1, sortDescriptors: [sortDescriptor], resultsHandler: { (query: HKSampleQuery?, results: [HKSample]?, err: NSError?) -> Void in
+                if err != nil {
+                    // ERROR Occurred, handled it
+                    print(err)
+                    return
+                }
+                var labelText = ""
+                for result in results as! [HKQuantitySample]! {
+                    // SUCCESS, use results here
+                    labelText += "\(result)"
+                }
+                self.displayData.text = labelText
+            }))
+            
+        }
+        
+        
+        
+        
+        
+        //
         
     }
 
